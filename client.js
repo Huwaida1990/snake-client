c4dab1f17cfa7bee169e841a68dc95b0105349e5
huwaida khalid <huwaida_khaild@yahoo.com>
2022-02-24 16:13:32 +0000

    smake

--- client.js
+++ client.js
@@ -1,3 +1,26 @@
-const conect =  require('./play') ;
-conect();
+const net = require('net');
+const { IP, PORT } = require('./constants');
 
+// establishes connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: RAY');
+  });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  
+  return conn;
+};
+
+//EXPORT
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,24 +1,5 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '10.0.2.15',
-    port:  50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on('connect',(msg)=>{
-    console.log("Successfully connected to game server");
-    conn.write('Name : __');
-  })
-
-
-  return conn;
-};
-
-console.log("Connecting ...");
-
-module.exports = connect;
+const { connect } = require('./client');
+const { setupInput } = require('./input');
+console.log('Connecting ...');
+const conn = connect();
+setupInput(conn);
